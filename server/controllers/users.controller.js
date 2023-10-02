4b716e877edbe23e4301f0dd744b4103c38d2437
LUCAS EDUARDO SASSE <lucas_sasse@estudante.sc.senai.br>
2023-10-02 20:39:18 -0300

    bug fixed

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -3,32 +3,48 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.postSignup = (req, res, next) => {
+
+
 	mysql.getConnection((error, conn) => {
-		if(error){ return res.status(500).send({error: error}) }
-		conn.query('SELECT * FROM users WHERE email = ?', [req.body.email], (error, result) => {
-			if(error){ return res.status(500).send({error: error}) }
-			if(result.length > 0){
-				res.status(409).send({message: 'User Already Exists'});
-			}else{
-				bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
-					if(errBcrypt){ return res.status(500).send({error: errBcrypt}) }
-					conn.query(`INSERT INTO users (email, password) VALUES (?, ?)`,
-						[req.body.email, hash],
-						(error, result) => {
-							conn.release();
-							if(error){ return res.status(500).send({error: error}) }
-							const response = {
-								message: 'User Created Successfully',
-								userCreated: {
-									id_user: result.insertId,
-									email: req.body.email
-								}
-							}
-							return res.status(201).send(response);
-						});
-				});
-			}
-		});
+		try {
+
+			conn.query('SELECT * FRM users',[], (error, result) => {
+				if (error) res.send(error);
+
+				res.status(200).send({
+					ok:result
+				})
+			})
+		}catch(error){
+			res.status(500).send({
+				message:error.message
+			})
+		}
+		// if(error){ return res.status(500).send({error: error}) }
+		// conn.query('SELECT * FROM users WHERE email = ?', [req.body.email], (error, result) => {
+		// 	if(error){ return res.status(500).send({error: error}) }
+		// 	if(result.length > 0){
+		// 		res.status(409).send({message: 'User Already Exists'});
+		// 	}else{
+		// 		bcrypt.hash(req.body.password, 10, (errBcrypt, hash) => {
+		// 			if(errBcrypt){ return res.status(500).send({error: errBcrypt}) }
+		// 			conn.query(`INSERT INTO users (email, password) VALUES (?, ?)`,
+		// 				[req.body.email, hash],
+		// 				(error, result) => {
+		// 					conn.release();
+		// 					if(error){ return res.status(500).send({error: error}) }
+		// 					const response = {
+		// 						message: 'User Created Successfully',
+		// 						userCreated: {
+		// 							id_user: result.insertId,
+		// 							email: req.body.email
+		// 						}
+		// 					}
+		// 					return res.status(201).send(response);
+		// 				});
+		// 		});
+		// 	}
+		// });
 	});
 };
 
