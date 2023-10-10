73e6ba32f8573d8b8a0364ea6da3308f4e798893
LUCAS EDUARDO SASSE <lucas_sasse@estudante.sc.senai.br>
2023-10-09 21:38:42 -0300

    adicao front facul

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,15 @@ const cors = require('cors');
 
 const app = express();
 const port = 3500;
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders:
+        "Content-Type,Authorization,X-Requested-With, content-type,XMLHttpRequest,Access-Control-Allow-Origin,Origin,Accept,Access-Control-Allow-Credentials,",
+};
+app.use(cors(corsOptions));
+
 app.use(bodyparser.json());
 
 app.use('/api', userRouter);
