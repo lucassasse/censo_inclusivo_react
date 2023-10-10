725bb5f816a165fed0d337c18d3e41419e1054ae
LUCAS EDUARDO SASSE <lucas_sasse@estudante.sc.senai.br>
2023-10-09 21:37:41 -0300

    adicao front facul

---
+++ client_aula/app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { LoginForm } from './pages/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginForm />} />
+          <Route path="home" element={<h1>Página HOME</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ client_aula/app/src/pages/LoginForm/LoginForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import {
+  VStack,
+  FormControl,
+  Input,
+  FormLabel,
+  Button,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
+import AuthService from '../../services/AuthService';
+
+export function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+    setError(false);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await AuthService.login(email, password);
+      sessionStorage.setItem('token', response.token);
+      window.location.href = '/home';
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <VStack spacing={3} width={300} margin="auto">
+      <FormControl id="email">
+        <FormLabel>E-mail</FormLabel>
+        <Input
+          type="email"
+          placeholder="Informe o email"
+          onChange={event => {
+            setEmail(event.target.value);
+          }}
+        ></Input>
+      </FormControl>
+
+      <FormControl id="password">
+        <FormLabel>Senha</FormLabel>
+        <Input
+          placeholder="Informe a senha"
+          type="password"
+          onChange={handlePasswordChange}
+        ></Input>
+      </FormControl>
+
+      <Button colorScheme="blue" onClick={handleLogin}>
+        Login
+      </Button>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>ERRO!</AlertTitle>
+          <AlertDescription>Erro ao tentar logar.</AlertDescription>
+        </Alert>
+      )}
+    </VStack>
+  );
+}
