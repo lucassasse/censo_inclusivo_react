7222d8d8364d19dce15187af9dcf7486395f7cfe
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2023-10-30 19:51:17 -0300

    update

--- client_mobile/src/pages/Login/Login.js
+++ client_mobile/src/pages/Login/Login.js
@@ -86,6 +86,18 @@ export function Login() {
 				</Button>
 			</Center>
 
+			<Center>
+				<Text
+					textAlign="center"
+					fontSize="lg"
+					mt="-2vh"
+				>
+					<Link href="#">
+						Esqueci minha senha
+					</Link>
+				</Text>
+			</Center>
+
 			{showAlert && (
 				<Alert
 					title="Erro de login"
@@ -100,6 +112,7 @@ export function Login() {
 					textAlign="center"
 					fontSize="2xl"
 					bold
+					mt="5vh"
 				>
 					Cadastre-se e ajude a construir benefícios para você!
 				</Text>
@@ -110,20 +123,10 @@ export function Login() {
 					w="90%"
 					size="lg"
 					bg="projectBlue.100"
+					mt="-1vh"
 				>
 					CADASTRAR
 				</Button>
-			</Center>
-
-			<Center>
-				<Text
-					textAlign="center"
-					fontSize="lg"
-				>
-					<Link href="#">
-						Esqueci minha senha
-					</Link>
-				</Text>
 			</Center>
 
 		</VStack>
